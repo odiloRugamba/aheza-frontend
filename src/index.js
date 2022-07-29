ba3f65cc3ed888e4c87f91553213b5b54b944b07
cyusaman <61387225+cyusaman@users.noreply.github.com>
2022-07-29 11:46:11 -0400

    now

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import { store } from "./store";
 
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
